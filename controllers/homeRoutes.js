82502c6c5a440514d3d77207bec212111d573af0
Todd Harvey <tharveyster@gmail.com>
2021-11-18 15:09:35 -0500

    Refactored to MVC format but haven't added new functionality

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req,res) => {
+  res.render('calendar');
+})
+
+router.get('/signin', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signin');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+})
+
+module.exports = router;
